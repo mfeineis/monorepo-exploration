0a2a9db3ac2e8d05f0dff1ec1ef9c64e83caf4a2
Martin Feineis <mfeineis@users.noreply.github.com>
2018-12-16 20:27:25 +0100

    chore(#1): Add repository scope `eslint` config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,35 @@
 module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:fp/recommended",
+        "plugin:import/recommended",
+        "plugin:ramda/recommended",
+    ],
+    plugins: [
+        "fp",
+        "import",
+        "ramda",
+    ],
+    root: true,
+    rules: {
+        "arrow-parens": 2,
+
+        "fp/no-mutation": [2, {
+            commonjs: true,
+        }],
+        "fp/no-nil": 1,
+        "fp/no-unused-expression": 1,
+
+        "import/no-unresolved": [2, {
+            ignore: [
+                "filament$",
+                "intl-elements$",
+            ]
+        }],
+    },
 };
